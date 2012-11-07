b9e772e62602266a29bb47a44b1f0346d8cfee6d
Mohamad Dwikuntobayu <mdwikuntobayu@gmail.com>
2012-11-07 15:35:08 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UjangRuby::Application.config.secret_token = 'dc5b877cf972bfb9df8a71e7b844cc0d62dc90b0dc2e35efe521b53854b32fabbeeee5df1a45edd2e1d7deb86f5db72fdd433ffcda1a3c6c129c8e100e7bbb05'
