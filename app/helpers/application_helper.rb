7b86871487bac38e5d36655c3c31f74d1a038245
Mohamad Dwikuntobayu <mdwikuntobayu@gmail.com>
2012-11-07 18:46:30 +0900

    add title on aplicationhelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#return a title on a per-page
+	def title
+		base_title = "Ujang Rails"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
